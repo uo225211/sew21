856c47939dba43e302e0e4f397ff9704ad8754cb
uo225211 <71693558+uo225211@users.noreply.github.com>
2021-11-30 12:27:29 +0100

    subir ej2

---
+++ MasInfoNavegador.js
@@ -0,0 +1,10 @@
+document.write("Version: ");
+document.write(infoNavegador.version);
+document.write(", Plataforma: ");
+document.write(infoNavegador.plataforma);
+document.write(", Vendedor: ");
+document.write(infoNavegador.vendedor);
+document.write(", Agente: ");
+document.write(infoNavegador.agente);
+document.write(", Java Activo: ");
+document.write(infoNavegador.javaActivo);
--- Titulo1.js
+++
@@ -1,8 +0,0 @@
-class Titulo1 {
-    constructor(){
-        document.write("<h1>");
-        document.write(asignatura.nombre);
-        document.write("</h1>");
-    }
-}
-var titulo1=new Titulo1();
